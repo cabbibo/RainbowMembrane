f9b8afc33a786991744944f22b275d4399a4f2b1
Isaac Cohen <isaaclandoncohen@gmail.com>
2015-01-06 15:33:33 -0800

    adding loops

--- Tone.js
+++ Tone.js
@@ -1,23 +1,35 @@
 
-function Tone( audioController ,  freq , position ){
+var TONE_GEO = new THREE.IcosahedronGeometry( .02 , 1 );
+var TONE_MAT = new THREE.MeshBasicMaterial({color:0xffffff});
+function Tone( audioController ,  freq , baseVal , position ){
 
   var ctx = audioController.ctx;
 
-  this.base   = ctx.createOscillator();
-  this.gain   = ctx.createGain();
-  this.panner = ctx.createPanner();
+  this.base       = ctx.createOscillator();
+  this.baseGain   = ctx.createGain();
+  this.gain       = ctx.createGain();
+  this.panner     = ctx.createPanner();
 
   this.base.frequency.value = freq;
   this.base.start( 0 );
 
-  this.base.connect( this.gain );
+  this.baseGain.gain.value = baseVal;
+
+  this.base.connect( this.baseGain );
+  this.baseGain.connect( this.gain );
   this.gain.connect( this.panner );
   this.panner.connect( audioController.gain );
 
+  //this.gain.connect( audioController.gain );
   this.position = position;
 
   var p = position;
   this.panner.setPosition( p.x , p.y , p.z );
+
+  this.mesh = new THREE.Mesh( TONE_GEO , TONE_MAT );
+  this.mesh.position.copy( p );
+
+  //scene.add( this.mesh );
  
   audioController.addToUpdateArray( this.update.bind( this )  );
 
@@ -33,7 +45,7 @@ Tone.prototype.update = function(){
   //
     tv1.copy( this.position );
     tv1.sub( soundPos );
-    this.gain.gain.value = Math.min( 1 , .1 / tv1.length() );
+    this.gain.gain.value = Math.min( 1 ,  1 - Math.pow(tv1.length(), .01 ) );
 
   }
 
--- initTones.js
+++ initTones.js
@@ -4,32 +4,88 @@ function initTones( val ){
   var v = val || 60;
 
   var tones = [];
+ 
+  var dir = [
+
+    [ 0 ,  0 , -1 ],
+    [ 0 ,  0 ,  1 ],
+    [ 1 ,  0 ,  0 ],
+    [-1 ,  0 ,  0 ],
+    [ 0 ,  1 ,  0 ],
+    [ 0 , -1 ,  0 ],
+
+  ]
+
+  var loops = [
+    'audio/box1.wav', 
+    'audio/box2.wav', 
+    'audio/box3.wav', 
+    'audio/box4.wav', 
+    'audio/box5.wav', 
+    'audio/box6.wav',
+    'audio/box7.wav',
+    'audio/box8.wav',
+    'audio/box9.wav',
+    'audio/box10.wav',
+    'audio/box11.wav',
+    'audio/box12.wav',
+    'audio/box13.wav',
+    'audio/box14.wav',
+    'audio/box15.wav',
+    'audio/box16.wav',
+  ]
+
+  var numOf = 4;
+
+  for( var i  = 0; i < 1; i++ ){
+
+    for( var j = 0; j < numOf; j++ ){
+      for( var k = 0; k < numOf; k++ ){
+
+        var d = dir[i];
+
+        var x= 0,y = 0,z = 0;
+        var freq = i 
+       
+        if( d[0] != 0 ){
+
+          x = d[0] * .3;
+          y = (((j+.5) - numOf/2 )/numOf) * .6; 
+          z = (((k+.5) - numOf/2 )/numOf) * .6; 
+
+
+        }else if(  d[1] != 0 ){
+
+          x = (((j+.5) - numOf/2 )/numOf) * .6; 
+          y = d[1] * .3;
+          z = (((k+.5) - numOf/2 )/numOf) * .6; 
+
+        }else if(  d[2] != 0 ){
+
+          x = (((j+.5) - numOf/2 )/numOf) * .6; 
+          y = (((k+.5) - numOf/2 )/numOf) * .6; 
+          z = d[2] * .3;
+
+        }
+
+
+        var v = (j+1) * 20;
+        var pos = new THREE.Vector3( x , y , z );
+
+        var loop = loops[ j * numOf + k];
+        neededToLoad++;
+        tones.push( new Loop( audioController , loop , .1 , pos ) );
+
+      }
+
+
+    }
+
+
+  }
+
+
   
-  var pos = new THREE.Vector3( .3 , .3 , .3 );
-  tones.push( new Tone(audioController , v , pos) );
-
-  var pos = new THREE.Vector3( -.3 , .3 , .3 );
-  tones.push( new Tone(audioController , v*2 , pos) );
-
-  var pos = new THREE.Vector3( .3 , -.3 , .3 );
-  tones.push( new Tone( audioController , v*3 , pos) );
-
-  var pos = new THREE.Vector3( .3 , .3 , -.3 );
-  tones.push( new Tone(audioController , v*4 , pos) );
-
-  var pos = new THREE.Vector3( .3 , -.3 , -.3 );
-  tones.push( new Tone(audioController , v*5 , pos) );
-
-  var pos = new THREE.Vector3( -.3 , .3 , -.3 );
-  tones.push( new Tone(audioController , v*6 , pos) );
-
-  var pos = new THREE.Vector3( -.3 , -.3 , .3 );
-  tones.push( new Tone(audioController , v*7 , pos) );
-
-  var pos = new THREE.Vector3( -.3 , -.3 , -.3 );
-  tones.push( new Tone(audioController , v*8 , pos) );
-
-
   return tones;
 
 }
